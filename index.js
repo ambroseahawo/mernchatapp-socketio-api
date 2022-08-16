dd4782609e4d2f1d4af919427791822bfeaa377c
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-16 21:43:05 +0300

    setup mongodb connection

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ app.use(express.json())
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () =>{
-  console.log("Backend server running")
+  mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(
+      () => console.log("Connected to MongoDB")
+    ).catch((error) => console.log(error.message))
 })
