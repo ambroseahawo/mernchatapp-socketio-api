935ef296df148e23c475cbe6b7e6b3c3e7c8993f
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-17 10:08:11 +0300

    Implement login user controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt"
 
 // register user
-export const registerNewUser = async(req, res) =>{
+export const registerNewUser = async (req, res) => {
   try {
     // generate new hashed password
     const salt = await bcrypt.genSalt(10)
@@ -21,6 +21,21 @@ export const registerNewUser = async(req, res) =>{
     res.status(200).json(other)
   } catch (error) {
     console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+// login existing user
+export const loginExistingUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    !user && res.status(404).json("User not found")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("Invalid Password. Please try again!")
+
+    res.status(200).json(user)
+  } catch (error) {
     res.status(500).json(error)
   }
 }
