5abe082071dc3ad05f46b0413568475e0aff83ba
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-17 10:22:24 +0300

    Implement login user route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { registerNewUser } from "../controllers/auth.js"
+import { registerNewUser, loginExistingUser } from "../controllers/auth.js"
 
 const router = express.Router()
 
 // register user route
 router.post("/register", registerNewUser)
+router.post("/login", loginExistingUser)
 
 export default router
