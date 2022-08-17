c47f3963240ab900bd41954975e49edcc3bf3389
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-17 14:29:00 +0300

    Implement chat members validation

--- controllers/chats.js
+++ controllers/chats.js
@@ -1,15 +1,32 @@
+import mongoose from "mongoose"
 import Chat from "../models/Chat.js"
 
 // create new chat
-export const newChat = async(req, res) =>{
+export const newChat = async (req, res) => {
   const newChat = new Chat({
     members: [req.body.senderId, req.body.receiverId]
   })
 
-  try{
+  try {
+    // check if the members array has only two items, distinct
+    const validMembers = (newChat.members.length === 2) &&
+      (new Set(newChat.members)).size === newChat.members.length
+
+    if(!validMembers){
+      return res.status(400).send(
+        "Private chat should have only two distinct members"
+      )
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(newChat.members[0])) {
+      return res.status(400).send("Invalid Member")
+    }
+    if (!mongoose.Types.ObjectId.isValid(newChat.members[1])) {
+      return res.status(400).send("Invalid Member")
+    }
     const savedChat = await newChat.save()
     res.status(200).json(savedChat)
-  }catch(error){
-    res.status(500).json(error)
+  } catch (error) {
+    res.status(500).json(error.message)
   }
 }
