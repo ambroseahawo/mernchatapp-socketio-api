6f8f84359d1eacf44f64dedab26d60e7a0f178fb
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-17 09:57:17 +0300

    Implement get user controller

---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+import User from "../models/User.js"
+
+// get user
+export const getUser = async(req, res) =>{
+  const userId = req.query.userId
+  // const username = req.query.username
+
+  try{
+    // validate id
+    if (!mongoose.Types.ObjectId.isValid(userId)){
+      return res.status(404).send(`No user record with id: ${userId}`)
+    }
+
+    const user = await User.findById(userId)
+    const { password, updatedAt, ...other } = user._doc
+    res.status(200).json(other)
+  }catch(error){
+    res.status(500).json(error)
+  }
+}
