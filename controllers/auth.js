8eeee53553ad2e5acb2e3f00c8291a0f17efb8fa
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-17 08:28:35 +0300

    Implement register user controller

---
+++ controllers/auth.js
@@ -0,0 +1,25 @@
+import User from "../models/User";
+import bcrypt from "bcrypt"
+
+// register user
+export const registerNewUser = async(req, res) =>{
+  try {
+    // generate new hashed password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    // create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+
+    // save new user with response
+    const user = await newUser.save()
+    res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
