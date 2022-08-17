aa2f4a78d1b985c8d8a4594a6d89f1a011fd3d36
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-17 22:10:09 +0300

    Validate Chat ID and Sender ID

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import Message from "../models/Message.js"
 
 // create new messages
@@ -5,9 +6,15 @@ export const createMessage = async(req, res) =>{
   const newMessage = new Message(req.body)
 
   try{
+    if (!mongoose.Types.ObjectId.isValid(req.body.chatId)) {
+      return res.status(400).send("Invalid Chat")
+    }
+    if (!mongoose.Types.ObjectId.isValid(req.body.senderId)) {
+      return res.status(400).send("Invalid Sender")
+    }
     const savedMessage = await newMessage.save()
     res.status(200).json(savedMessage)
   }catch(error){
-    res.status(500).json(error)
+    res.status(500).json(error.message)
   }
 }
