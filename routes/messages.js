04c477e7ff8c2bf4c10c519ecfe442d021d63e27
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-18 09:16:25 +0300

    Implement get existing chat messages route

--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { createMessage } from "../controllers/messages.js"
+import { createMessage, getMessages } from "../controllers/messages.js"
 
 const router = express.Router()
 
+router.get("/:chatId", getMessages)
 router.post("/", createMessage)
 
 export default router
