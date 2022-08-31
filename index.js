f842a4d0d858bb326790f5255f05997ee74c9a55
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-31 21:29:02 +0300

    Implement socketio on node server

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const app = express()
 import mongoose from "mongoose"
 import { config } from "dotenv"
 import cors from "cors"
+import http from "http"
+import { Server } from "socket.io"
 
 import authRoutes from "./routes/auth.js"
 import usersRoutes from "./routes/users.js"
 import chatsRoutes from "./routes/chats.js"
 import messagesRoutes from "./routes/messages.js"
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_BASE_URL
+  }
+})
 
 config()
 
@@ -19,9 +28,52 @@ app.use("/api/users", usersRoutes)
 app.use("/api/chats", chatsRoutes)
 app.use("/api/messages", messagesRoutes)
 
+let users = []
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId })
+}
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId)
+}
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId == userId)
+}
+
+io.on("connection", (socket) => {
+  // when connect
+  console.log(`A user connected on ${socket.id}`)
+  // io.emit("welcome", "hello, this is socket server!") // sends to all users connected
+
+  // sends to specific user connected
+  // take userid and socketid from user
+  socket.on("addUser", userId => {
+    addUser(userId, socket.id)
+    io.emit("getUsers", users)
+  })
+
+  // send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId)
+    io.to(user.socketId).emit("getMessage", {
+      senderId, text
+    })
+  })
+
+  // when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id)
+    io.emit("getUsers", users)
+  })
+})
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () =>{
+server.listen(PORT, () => {
   mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true }).then(
