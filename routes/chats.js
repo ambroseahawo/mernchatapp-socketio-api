ddb8a8f608ec446c4c83e44f662465d3a4448fa2
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-09-05 21:20:14 +0300

    Implement current chat routes

--- routes/chats.js
+++ routes/chats.js
@@ -1,10 +1,13 @@
 import express from "express"
 import { getChat, newChat } from "../controllers/chats.js"
 import { getChatUsers } from "../controllers/chatUsers.js"
+import { getCurrentChat, postCurrentChat } from "../controllers/currentChat.js"
 
 const router = express.Router()
 
 router.get("/users", getChatUsers)
+router.post("/current", postCurrentChat)
+router.get("/current", getCurrentChat)
 router.get("/:userId", getChat)
 router.post("/", newChat)
 
